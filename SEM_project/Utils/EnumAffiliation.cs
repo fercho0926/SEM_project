23803cb314e8404b01236c04f97c7d49e1a3044c
jottagithub <jaristizabal10@hotmail.com>
2023-10-13 13:46:54 -0500

    sdf

--- SEM_project/Utils/EnumAffiliation.cs
+++ SEM_project/Utils/EnumAffiliation.cs
@@ -1,10 +1,14 @@
-﻿namespace SEM_project.Utils
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEM_project.Utils
 {
     public enum EnumAffiliation
     {
+        [Display(Name = "Carrera Administrativa")]
         Carrera_Administrativa,
         Provisional,
         Contratista,
+        [Display(Name = "Libre remoción y nombramiento")]
         Libre_Remocion_Y_Nombramiento,
         NA
     }
--- SEM_project/Utils/EnumSubdepartment.cs
+++ SEM_project/Utils/EnumSubdepartment.cs
@@ -1,9 +1,14 @@
-﻿namespace SEM_project.Utils
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEM_project.Utils
 {
     public enum EnumSubdepartment
     {
+        [Display(Name = "Administrativa y Financiera")]
         Administrativa_Financiera,
+        [Display(Name = "Planeación Educativa")]
         Planeacion_Educativa,
+        [Display(Name = "Prestación del servicio Educativo")]
         Prestación_del_Servicio_Educativo,
         NA
     }
